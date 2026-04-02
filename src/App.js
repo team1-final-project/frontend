b6bf18ac7fdc911a5d70e70cc768b7f3fd46bcc9
hyunhyunhyun1207 <bugsteam9912@gmail.com>
2026-04-02 15:12:14 +0900

    [fix] App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
-import theme from "./styles/theme";
+import theme from "./styles/Theme";
 import { AuthProvider } from "./context/AuthContext";
 import GoogleLoginCallbackPage from "./test/GoogleLoginCallbackPage";
 
@@ -13,22 +13,20 @@ import AuthTestPage from "./test/AuthTestPage";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <AuthProvider>
-          <GlobalStyle />
-          <Routes>
-            <Route path="/" element={<AuthTestPage />} />
-            <Route
-              path="/auth/google/callback"
-              element={<GoogleLoginCallbackPage />}
-            />
-            <Route path="*" element={<Navigate to="/" replace />} />
+      <AuthProvider>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<AuthTestPage />} />
+          <Route
+            path="/auth/google/callback"
+            element={<GoogleLoginCallbackPage />}
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
