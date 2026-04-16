eaa12cfcc646589b13ef438bb248d4768ad6e4ea
lense1 <star_ruddks@naver.com>
2026-04-16 12:36:24 +0900

    [fix] 안전재고 수정

--- src/pages/admin/products/ProductUpdate.js
+++ src/pages/admin/products/ProductUpdate.js
@@ -735,7 +735,6 @@ export default function ProductUpdate() {
                     onChange={(e) =>
                       handleChange("safetyStock", e.target.value)
                     }
-                    readOnly
                   />
                   <UnitText>개</UnitText>
                 </UnitInputWrap>
